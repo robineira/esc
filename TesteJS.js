d5d905660d127b8829d451b8e72c5a2fcaa75f78
robineira <59578589+robineira@users.noreply.github.com>
2020-01-06 16:23:44 -0300

    Create TesteJS.js

---
+++ TesteJS.js
@@ -0,0 +1,25 @@
+function AlteraRele1(){
+    var estado = document.getElementById("estado").innerHTML;
+    if(estado === "0"){
+        document.getElementById("estado").innerHTML="1";
+        document.getElementById("rele").innerHTML="<div class='rele1_ligado'></div>";
+		document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>DESLIGA</a>";
+    } else {
+        document.getElementById("estado").innerHTML="0";
+        document.getElementById("rele").innerHTML="<div class='rele1_desligado'></div>";
+        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>LIGA</a>";
+    }
+}
+
+function AlteraRele2(){
+    var estado_2 = document.getElementById("estado_2").innerHTML;
+    if(estado_2 === "0"){
+        document.getElementById("estado_2").innerHTML="1";
+        document.getElementById("rele_2").innerHTML="<div class='rele2_ligado'></div>";
+		document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_2'>DESLIGA</a>";
+    } else {
+        document.getElementById("estado_2").innerHTML="0";
+        document.getElementById("rele_2").innerHTML="<div class='rele2_desligado'></div>";
+        document.getElementById("botao_2").innerHTML="<a href='/?2_ligar' class='botao_2'>LIGA</a>";
+    }
+}
